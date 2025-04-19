b8c8259ef48be1cd8004e71882486cad392ae37a
JonathanAndresM <166529327+JonathanAndresM@users.noreply.github.com>
2025-04-18 21:02:22 -0300

    "Arreglos para git"

--- events/ready.js
+++
@@ -1,9 +0,0 @@
-export default {
-  // Este evento se activa cuando el bot está listo y conectado
-    name: 'ready',
-    once: true,
-    execute(client) {
-      console.log(`✅ Bot conectado como ${client.user.tag}`);
-    },
-  };
-  
