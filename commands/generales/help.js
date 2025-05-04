f415c78a745faf4960632a279ec9b2ba214d93c7
Yoel <yoeldelbuono@outlook.com>
2025-05-04 12:50:37 -0300

    refactor: change name from ayuda file to help

--- commands/generales/help.js
+++ commands/generales/help.js
@@ -1,6 +1,8 @@
-export const help = async (interaction) => {
+const help = async (interaction) => {
 	await interaction.reply({
 		content: '**📌 Pasos para registrarte:**\n1. Verificá tu email\n2. Completá tu perfil con `/perfil`\n3. ¡Listo!',
 		flags: 64,
 	})
 }
+
+export default help
